1f065a3dc5e0f1f987c6d76dc738d3e2cb5508c0
vstolyarov <Vik13.94@mail.ru>
2018-05-28 21:53:14 +0300

    Make 1 exercise

--- ex1.php
+++ ex1.php
@@ -5,3 +5,10 @@
  * Date: 28.05.2018
  * Time: 17:40
  */
+$nums = explode(',', $argv[1]);
+echo $nums[0];
+for($i = 0; $i < count($nums) - 1; $i++){
+    do{
+        echo ','.++$nums[$i];
+    } while ( isset( $nums[ $i + 1 ] ) && $nums[ $i ] != $nums[ $i + 1 ] );
+}
